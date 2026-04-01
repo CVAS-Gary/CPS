d7cf58c0b5967df5d966084ee9cf0d1e43524ac6
CVAS-Gary <gary.tsai@cloudriches.com>
2026-04-01 15:57:11 +0800

    Updated

--- Python/demo.py
+++ Python/demo.py
@@ -1,16 +1,2 @@
-# 由 GitHub Copilot 產生
-# 本建議未參考任何 GitHub 原始碼
-
-def fizzbuzz(N):
-    for I in range(1, N + 1):
-        if I % 15 == 0:
-            print("FizzBuzz")
-        elif I % 3 == 0:
-            print("Fizz")
-        elif I % 5 == 0:
-            print("Buzz")
-        else:
-            print(I)
-if __name__ == "__main__":
-    N = int(input("Enter a number: "))
-    fizzbuzz(N)
+def add(a, b):
+    return a - b
